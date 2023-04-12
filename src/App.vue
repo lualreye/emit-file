1016847fde83e199f4ee1499aa1716c2500147fa
Luis Reyes <lualreye@gmail.com>
2023-04-11 21:02:00 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { ref } from 'vue';
+import UploadImage from './components/UploadImage.vue';
+
+const image = ref('')
+
+function getImage(file) {
+  const url = URL.createObjectURL(file)
+  image.value = url
+}
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <UploadImage @pass-image="getImage" />
+  <img
+    v-if="image.length"
+    :src="image"
+    class="image"
+    alt="example">
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+.image {
+  margin-top: 24px;
+  width: 300px;
+  height: 240px;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
---
+++ src/components/UploadImage.vue
@@ -0,0 +1,49 @@
+<script setup>
+const emit = defineEmits(['pass-image']);
+
+function onFileChange(event) {
+  const file = event.target.files[0];
+  emit('pass-image', file)
+}
+
+</script>
+
+<template>
+  <div class="input-wrapper">
+    <input
+      type="file"
+      class="input"
+      accept="image/*"
+      @change="onFileChange">
+    <div class="fake-input">
+      Subir archivo
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.input-wrapper {
+  width: 120px;
+  padding: 4px 8px;
+  position: relative;
+  margin: auto;
+}
+.input {
+  opacity: 0;
+  cursor: pointer;
+}
+.fake-input {
+  width: 100%;
+  height: 100%;
+  border-radius: 4px;
+  background-color: #41b883;
+  color: white;
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  pointer-events: none;
+}
+</style>
